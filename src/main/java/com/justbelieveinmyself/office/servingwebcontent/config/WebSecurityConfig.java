6b48603bc2f92579992ef90b3a4d7b1f010170fb
justbelieveinmyself <122427316+justbelieveinmyself@users.noreply.github.com>
2023-08-22 20:18:57 +0300

    Remember me feature added

--- src/main/java/com/justbelieveinmyself/office/servingwebcontent/config/WebSecurityConfig.java
+++ src/main/java/com/justbelieveinmyself/office/servingwebcontent/config/WebSecurityConfig.java
@@ -1,16 +1,23 @@
 package com.justbelieveinmyself.office.servingwebcontent.config;
 
 import com.justbelieveinmyself.office.servingwebcontent.service.JpaUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.RememberMeServices;
+import org.springframework.security.web.authentication.rememberme.RememberMeAuthenticationFilter;
+import org.springframework.security.web.authentication.rememberme.TokenBasedRememberMeServices;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
@@ -20,8 +27,11 @@ public class WebSecurityConfig {
     public WebSecurityConfig(JpaUserDetailsService jpaUserDetailsService) {
         this.jpaUserDetailsService = jpaUserDetailsService;
     }
-
+    @Lazy
     private final JpaUserDetailsService jpaUserDetailsService;
+//    @Autowired
+//    @Lazy
+//    private RememberMeServices rememberMeServices;
     private AntPathRequestMatcher[] antPathRequestMatchers = new AntPathRequestMatcher[]{
             AntPathRequestMatcher.antMatcher(HttpMethod.POST,"/registration"),
             AntPathRequestMatcher.antMatcher(HttpMethod.GET,"/registration"),
@@ -40,6 +50,7 @@ public class WebSecurityConfig {
                         .requestMatchers(antPathRequestMatchers).permitAll()
                         .anyRequest().authenticated())
                 .userDetailsService(jpaUserDetailsService)
+                .rememberMe(Customizer.withDefaults())
                 .formLogin((form) -> form
                         .loginPage("/login").permitAll()
                 ).logout((logout) -> logout.permitAll());
@@ -47,5 +58,16 @@ public class WebSecurityConfig {
         return http.exceptionHandling(exceptionHandling -> exceptionHandling.accessDeniedHandler(((request, response, accessDeniedException) -> accessDeniedException.printStackTrace()))).build();
     }
 
-
+//    @Bean
+//    RememberMeServices getRememberMeServices(){
+//        TokenBasedRememberMeServices rememberMeServices = new TokenBasedRememberMeServices("spring", jpaUserDetailsService);
+//        rememberMeServices.s
+//        rememberMeServices.setTokenValiditySeconds(120);
+//        return rememberMeServices;
+//    }
+//    @Bean
+//    RememberMeAuthenticationFilter rememberMeAuthenticationFilter(){
+//        RememberMeAuthenticationFilter rememberMeFilter = new RememberMeAuthenticationFilter();
+//
+//    }
 }
