6ae9d84e40b7db71fd61110e9b1f7186b9c3ec98
justbelieveinmyself <122427316+justbelieveinmyself@users.noreply.github.com>
2023-09-15 22:38:09 +0300

    Preparation for paginition, rename some modeltags

--- src/main/java/com/justbelieveinmyself/office/servingwebcontent/repos/MessageRepository.java
+++ src/main/java/com/justbelieveinmyself/office/servingwebcontent/repos/MessageRepository.java
@@ -2,13 +2,17 @@ package com.justbelieveinmyself.office.servingwebcontent.repos;
 
 import com.justbelieveinmyself.office.servingwebcontent.domain.Message;
 import com.justbelieveinmyself.office.servingwebcontent.domain.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+
 
 import java.util.List;
 
 @Repository
 public interface MessageRepository extends CrudRepository<Message, Long> {
     List<Message> findByAuthor(User author);
-    List<Message> findByTag(String tag);
+    Page<Message> findByTag(String tag, Pageable pageable);
+    Page<Message> findAll(Pageable pageable);
 }
